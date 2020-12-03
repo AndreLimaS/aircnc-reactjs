f273c9f5d6dc015a799990d31b23ab19bb59d75b
andre <a.s.l0@hotmail.com>
2020-12-02 22:23:35 -0300

    first commit back-end

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const routes = require("./routes");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://aircnc:aircnc@aircnc.tjip1.mongodb.net/semana09?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+//req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edicao, delete)
+// req.body = Acessar corpo da requisicao (para criacao, edicao)
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
