5033623f8a27a8408b812d965976a8e6a46f25a1
andre <a.s.l0@hotmail.com>
2020-12-05 14:23:48 -0300

    feat: created component new and style

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,7 +1,66 @@
 import React from "react";
 
+import camera from "../../assets/camera.svg";
+import "./styles.css";
+
 function New() {
-  return <div></div>;
+  const [thumbnail, setThumbnail] = React.useState(null);
+  const [company, setCompany] = React.useState("");
+  const [techs, setTechs] = React.useState("");
+  const [price, setPrice] = React.useState("");
+
+  const preview = React.useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail]);
+
+  function handleSubmit() {}
+  return (
+    <form onSubmit={handleSubmit}>
+      <label
+        id="thumbnail"
+        style={{ backgroundImage: `url(${preview})` }}
+        className={thumbnail ? "has-thumbnail" : ""}
+      >
+        <input
+          type="file"
+          onChange={(event) => setThumbnail(event.target.files[0])}
+        />
+        <img src={camera} alt="Select img" />
+      </label>
+
+      <label htmlFor="company">EMPRESA *</label>
+      <input
+        id="company"
+        placeholder="Sua empresa incrivel"
+        value={company}
+        onChange={(event) => setCompany(event.target.value)}
+      />
+
+      <label htmlFor="techs">
+        TECNOLOGIAS *<span>(separadas por virgula)</span>
+      </label>
+      <input
+        id="techs"
+        placeholder="Quais tecnologias usam?"
+        value={techs}
+        onChange={(event) => setTechs(event.target.value)}
+      />
+
+      <label htmlFor="price">
+        VALOR DA DIARIA *<span>(em branco para GRATUITO)</span>
+      </label>
+      <input
+        id="price"
+        placeholder="Valor cobrado por dia"
+        value={price}
+        onChange={(event) => setPrice(event.target.value)}
+      />
+
+      <button type="submit" className="btn">
+        Cadastrar
+      </button>
+    </form>
+  );
 }
 
 export default New;
